35aaf6d78ecde9af3a8d6b86855515c6b3da4ea9
MohammadZaid-Saiyed <mozaidsaiyed8@gmail.com>
2024-10-31 11:46:33 +0530

    parctice set 6 & Exercise 2

---
+++ prac_set_6.js
@@ -0,0 +1,29 @@
+// Problem 1 & 2 & 3
+// let runAgain = true;
+// while (runAgain) {
+//   let age = prompt("Enter your age ");
+//   age = Number.parseInt(age);
+//   if (age <= 0) {
+//     alert("invalid age");
+//     break;
+//   }
+//   else if(age >= 4)
+//   else if (age >= 18) {
+//     alert("You can drive a Car");
+//   } else {
+//     alert("You can't drive a Car");
+//   }
+//   runAgain = confirm("Do You wanna play again");
+// }
+
+// Problem 4
+// let num = prompt("Enter a Number");
+// if (num >= 4) {
+//   location.href = "https://google.com";
+// } else {
+//   alert("Nothig !");
+// }
+
+// Problem 5
+let color = prompt("Enter the Background Color");
+document.body.style.background = color;
