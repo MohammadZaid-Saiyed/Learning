e6150fde07d22fc462483f48e9ae7b105807092c
MohammadZaid-Saiyed <mozaidsaiyed8@gmail.com>
2024-11-04 11:23:40 +0530

    className classList & insertAdjacent

---
+++ className_classList.js
@@ -0,0 +1,5 @@
+// first.className = "text-black red";   // to edit the class name
+// first.classList.add("red");   // to add the class
+// first.classList.remove("red");   // to remove the class
+// first.classList.contains("red");   // to check if it is presents
+// first.classList.toggle("red");   // adds the class if it doesn't exists, otherwise removes it
---
+++ insertAdjacent.js
@@ -0,0 +1,7 @@
+// inserting elements at different postions
+first.insertAdjacentHTML("beforebegin", '<div class="test">beforebegin</div>');
+first.insertAdjacentHTML("beforeend", '<div class="test">beforeend</div>');
+first.insertAdjacentHTML("afterbegin", '<div class="test">afterbegin</div>');
+first.insertAdjacentHTML("afterend", '<div class="test">afterend</div>');
+
+//first.remove(); // to remove the node. there's a method node.remove()
