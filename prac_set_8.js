6592ae8ff31c09aaf0e75dd0570c79de1db2ef69
MohammadZaid-Saiyed <mozaidsaiyed8@gmail.com>
2024-11-05 11:49:38 +0530

    Practice Set 8

---
+++ prac_set_8.js
@@ -0,0 +1,12 @@
+// Problem 3
+let a = document.getElementById("google");
+a.onclick = () => {
+  location.href = "https://google.com";
+};
+
+// Problem 4 is not here bcz this is not the part of this set
+
+// Problem 5
+// setInterval(function () {
+//   document.querySelector("#bulb").classList.toggle("bulb");
+// }, 300);
